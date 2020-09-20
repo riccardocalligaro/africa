2123335158215db08204d41f2684ecb16e6dde64
Riccardo Calligaro <riccardocalligaro@gmail.com>
2020-09-20 18:11:49 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Riccardo on 20/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
